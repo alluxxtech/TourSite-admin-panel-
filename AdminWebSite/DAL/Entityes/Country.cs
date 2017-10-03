f27a4206e3319506b6e2987503d8e85951023f9a
Sergiy <slevin.uk@gmail.com>
2017-10-03 18:15:02 +0300

    Start project

---
+++ AdminWebSite/DAL/Entityes/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdminWebSite.DAL.Entityes
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(maximumLength:256)]
+        public string Name { get; set; }
+        public DateTime DataCreate { get; set; }
+
+        public int Priority { get; set; }
+    }
+}
---
+++ AdminWebSite/Models/CountryViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdminWebSite.Models
+{
+    public class CountryViewModel
+    {
+        [Display(Name="Код краины")]
+        public int Id { get; set; }
+        [Display(Name="Назва")]
+        public string Name { get; set; }
+        [Display(Name = "Дата створення")]
+        public DateTime DateCreate { get; set; }
+        [Display(Name = "Приоритет")]
+        public int Priority { get; set; }
+    }
+
+    public class CountryCreateViewModel
+    {
+        
+        [Display(Name = "Назва")]
+        public string Name { get; set; }
+        
+        [Display(Name = "Приоритет")]
+        public int Priority { get; set; }
+    }
+}
